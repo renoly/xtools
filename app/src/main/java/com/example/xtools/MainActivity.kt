e710d1afe717610c71e3d527c150c08e28a37023
renoly <2045973656@qq.com>
2025-08-30 10:24:31 +0800

    删除无用依赖

--- app/src/main/java/com/example/xtools/MainActivity.kt
+++ app/src/main/java/com/example/xtools/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.xtools
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
